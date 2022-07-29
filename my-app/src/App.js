8bd6b1a2a11aed0a146fdd651c8ce05decdc0b38
xuanphong01bn <donhatphong94@gmail.com>
2022-07-29 10:51:01 +0700

    hehe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  let name = 'phong';
+  let obj = { name: 'Phong', Age: '21' };
+  let link = 'https://www.facebook.com/profile.php?id=100004722139213';
   return (
     <div className="App">
+      {console.log('check obj: ', obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world from Phong</h1>
-
+        <h1>Hello world from {typeof (name)} {obj.name}</h1>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target="_blank"> Visit my facebook</a>
       </header>
 
     </div>

18cb422adc349c8484421cb38253910c0b714165
xuanphong01bn <donhatphong94@gmail.com>
2022-07-29 14:19:07 +0700

    hehe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './views/Nav';
 const App = () => {
   let name = 'phong';
   let obj = { name: 'Phong', Age: '21' };
   let link = 'https://www.facebook.com/profile.php?id=100004722139213';
+  const handleClick = (event) => {
+    alert('hehe');
+    alert(event);
+  }
   return (
     <div className="App">
+      <Nav></Nav>
       {console.log('check obj: ', obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world from {typeof (name)} {obj.name}</h1>
-        <p>{JSON.stringify(obj)}</p>
-        <a href={link} target="_blank"> Visit my facebook</a>
+        <p style={{ color: 'red' }}>{JSON.stringify(obj)}</p>
+        <a href={link} target="_blank"> Visit Facebook</a>
+        <button onClick={(event) => handleClick(event)}>Click me</button>
       </header>
 
     </div>
   );
 }
+
 
 export default App;
---
+++ my-app/src/views/Nav.js
@@ -0,0 +1,13 @@
+import "./Nav.css"
+const Nav = () => {
+    return (
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+
+    )
+}
+export default Nav;
